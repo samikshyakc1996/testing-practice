8034a2d60e271ea1f9c66c864b035f590324e6cf
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-11-15 00:48:28 -0500

    calc program successfully passed add and subtract operation

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,8 +1,20 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function calc() {
+function sum(num1 = 0, num2 = 0) {
+  return num1 + num2;
+}
+function difference(num1 = 0, num2 = 0) {
+  return num1 - num2;
+}
 
+function calc(operation, num1, num2) {
+  switch (operation) {
+    case "add":
+      return sum(num1, num2);
+    case "subtract":
+      return difference(num1, num2);
+  }
 }
 
 // ==================================================
@@ -13,13 +25,21 @@ try {
   // Test Case 1
   // --------------------------------------------------
   // It should return the correct sum when the user provides: 'add', 1, 1.
-  var result = calc('add', 1, 1);
-  if (result !== 2) throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
+  var result = calc("add", 1, 1);
+  console.log("The answer of calc('add', 1, 1) is: " + result);
+  if (result !== 2)
+    throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
+  var result = calc("subtract", 20, 10);
+  console.log("The answer of calc('subtract', 20, 10) is: " + result);
+  if (result !== 10)
+    throw new Error(
+      'Expected calc("subtract", 20, 10) to be 10. Received: ' + result
+    );
 
   // --------------------------------------------------
   // Test Case 3
@@ -36,12 +56,12 @@ try {
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
 
-  console.log('All tests passed successfully.');
+  console.log("All tests passed successfully.");
 
-// ==================================================
-// PRINT ERRORS
-// ==================================================
+  // ==================================================
+  // PRINT ERRORS
+  // ==================================================
 } catch (e) {
-  console.warn('Whoops, the following test did not pass:');
+  console.warn("Whoops, the following test did not pass:");
   console.error(e.message);
 }
